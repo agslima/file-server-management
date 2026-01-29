96a0c7e08445160132b20437d4ba1c6830cecf76
Agnaldo Silva Lima <a.agnaldosilva@gmail.com>
2026-01-29 10:05:09 -0300

    Add authz coverage for gRPC methods

--- file-engine/internal/authz/method_map.go
+++ file-engine/internal/authz/method_map.go
@@ -4,8 +4,12 @@ import "github.com/example/file-engine/internal/auth"
 
 // MethodPermission maps gRPC full method name -> required permission.
 var MethodPermission = map[string]auth.Permission{
-    "/fileengine.FileEngine/CreateFolder":   auth.PermWrite,
-    "/fileengine.FileEngine/ListObjects":    auth.PermList,
-    "/fileengine.FileEngine/UploadObject":   auth.PermWrite,
-    "/fileengine.FileEngine/DownloadObject": auth.PermRead,
+	"/fileengine.FileEngine/CreateFolder":   auth.PermWrite,
+	"/fileengine.FileEngine/InitiateUpload": auth.PermWrite,
+	"/fileengine.FileEngine/CompleteUpload": auth.PermWrite,
+	"/fileengine.FileEngine/GetTaskStatus":  auth.PermRead,
+	"/fileengine.FileEngine/GetTask":        auth.PermRead,
+	"/fileengine.FileEngine/ListObjects":    auth.PermList,
+	"/fileengine.FileEngine/UploadObject":   auth.PermWrite,
+	"/fileengine.FileEngine/DownloadObject": auth.PermRead,
 }
